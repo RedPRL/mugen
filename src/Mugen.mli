17cd4787b57daf32e2da50225ee0be9a06ea8f95
favonia <favonia@gmail.com>
2022-07-07 19:16:26 -0500

    docs: document top-level modules

--- src/Mugen.mli
+++ src/Mugen.mli
@@ -1,6 +1,17 @@
+(** Structured types used in this library *)
 module StructuredType : module type of StructuredType
+
+(** Displacement algebras *)
 module Shift : module type of Shift
+
+(** Displacement algebras with joins *)
 module ShiftWithJoin : module type of ShiftWithJoin
+
+(** Syntax of universe levels with displacements *)
 module Syntax : module type of Syntax
+
+(** Smart builders for universe levels with displacements *)
 module Builder : module type of Builder
+
+(** Comparators for universe levels with displacements *)
 module Theory : module type of Theory
