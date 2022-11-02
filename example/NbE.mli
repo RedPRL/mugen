7d1ccd44d311da13961e2447852bf468898eb876
favonia <favonia@gmail.com>
2022-11-01 20:58:01 -0500

    docs: add more documentation to the example

--- example/NbE.mli
+++ example/NbE.mli
@@ -1,3 +1,5 @@
+(** NbE-based conversion checker for bidirectional type checking. *)
+
 val eval : Domain.env -> Syntax.t -> Domain.t
 val quote : int -> Domain.t -> Syntax.t
 val equate : int -> Domain.t -> Domain.t -> unit
